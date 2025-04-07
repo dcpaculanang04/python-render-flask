9af10929cc0e741a7a3b3629286626dfabcde8ab
Dale Brian C. Paculanang <dcpaculanang.student@asiancollege.edu.ph>
2025-04-07 21:47:53 +0800

    Update app.py
    commit

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def hello_world():
-   return 'Hello World'
+   return "<h1> Welcome To Render Flask,/h1>"
 
 if __name__ == '__main__':
    app.run()
